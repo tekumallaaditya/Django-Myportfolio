acfcdeef284e8f338a2537e31e5e777743ea9eb0
Aditya Tekumalla <tekumallaaditya@gmail.com>
2019-09-12 01:11:08 +0200

    finished navbar

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,3 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#Below is the media url and root
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
